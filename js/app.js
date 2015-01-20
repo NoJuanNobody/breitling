2728c786d8eeebca931dcf69aed4707bfc66143d
Alejandro Londonogit config --global user.email alondono11@gmail.comcdgit config --;istgit config --global user.name Alejandro <alondono11@gmail.com>
2015-01-20 18:01:02 -0500

    first commit

---
+++ js/app.js
@@ -0,0 +1,11 @@
+$(function(){
+
+$.vegas('slideshow', {
+  backgrounds:[
+    { src:'images/affinity.jpeg', fade:5000 },
+    { src:'images/cinelli.jpeg', fade:5000},
+    { src:'images/people.jpeg' ,fade:5000}
+  ]
+});
+
+});
